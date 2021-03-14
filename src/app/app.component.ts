14644e0fddc8da5b78af7f5f2aac054076d3171c
burakergun394 <burakergun394@gmail.com>
2021-03-14 16:49:13 +0300

    Adding Angular, Product, Navigation, CategoryComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'nortwhind';
+  title: string = 'nortwhind';
+  user: string = 'Burak Ergün';
 }
+
+
